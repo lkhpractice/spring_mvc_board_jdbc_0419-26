df4fb412e87ddabee0ac29f8d56972ca3614a4f0
ici <ici@DESKTOP-CL6HLKL>
2023-04-19 17:40:35 +0900

    백업

--- src/main/java/com/lkhpractice/board/command/BWriteCommand.java
+++ src/main/java/com/lkhpractice/board/command/BWriteCommand.java
@@ -1,12 +1,27 @@
 package com.lkhpractice.board.command;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.ui.Model;
+
+import com.lkhpractice.board.dao.BDao;
 
 public class BWriteCommand implements BCommand {
 
 	@Override
 	public void execute(Model model) {
 		
+		Map<String, Object> map = model.asMap();
+		HttpServletRequest request = (HttpServletRequest) map.get("request");
+		
+		String bname = request.getParameter("bname");
+		String btitle = request.getParameter("btitle");
+		String bcontent = request.getParameter("bcontent");
+		
+		BDao dao = new BDao();
+		dao.write(bname, btitle, bcontent);
 	}
 
 }
