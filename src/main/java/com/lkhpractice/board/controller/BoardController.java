e291c02d5693a74474a6a0413db68693e5b1684e
ici <ici@DESKTOP-CL6HLKL>
2023-04-20 09:34:10 +0900

    백업

--- src/main/java/com/lkhpractice/board/controller/BoardController.java
+++ src/main/java/com/lkhpractice/board/controller/BoardController.java
@@ -1,14 +1,30 @@
 package com.lkhpractice.board.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.lkhpractice.board.command.BWriteCommand;
 
 @Controller
 public class BoardController {
 
 	@RequestMapping(value = "/write_form")
-	public String write() {
+	public String write_form() {
 		
 		return "writeForm";
+	}
+	
+	@RequestMapping(value = "/write")
+	public String write(HttpServletRequest request, Model model) {
+		
+		model.addAttribute("request", request);
+		
+		BWriteCommand command = new BWriteCommand();
+		command.execute(model);
+		
+		return "";
 	}
 }
